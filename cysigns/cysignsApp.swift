7136d78100111bdba9217014468aaac0107a9bdb
Ilya Lyasin <lyasin.i@mail.ru>
2024-04-02 14:06:12 +0300

    Initial Commit

---
+++ cysigns/cysignsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cysignsApp.swift
+//  cysigns
+//
+//  Created by Ilia Liasin on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct cysignsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
