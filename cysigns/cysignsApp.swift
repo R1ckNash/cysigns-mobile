821de0c94cc5d6bee55e52247ae5c2a1185affa8
Ilya Lyasin <lyasin.i@mail.ru>
2024-04-15 18:44:31 +0300

    update UI

--- cysigns/cysignsApp.swift
+++ cysigns/cysignsApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct cysignsApp: App {
+    
     var body: some Scene {
         WindowGroup {
             QuizView()
+                .environment(\.colorScheme, .light)
         }
     }
 }
